b2d8c0d69c03f2a567939b4cad60d63f882cc0d0
Giulia <Giulia@192.168.1.207>
2021-11-12 17:37:39 +0100

    Final version for today 12/11

--- MyPrjGH/src/com/business/GitTest.java
+++ MyPrjGH/src/com/business/GitTest.java
@@ -3,7 +3,8 @@ package com.business;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Base Version");
-		}
+		System.out.println("Base Version - 2");
+		
+	}
 
 }
